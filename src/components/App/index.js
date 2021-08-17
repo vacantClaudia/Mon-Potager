aa88c835a5468e779534096146ac5db86124f83b
Fouzia33 <fouzia.hollande@gmail.com>
2021-08-17 11:59:16 +0200

    initialisation du composant App

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,14 +2,13 @@
 import React from 'react';
 
 // == Import
-import reactLogo from './react-logo.svg';
 import './styles.css';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+    <h1>Header</h1>
+    <h1>Footer</h1>
   </div>
 );
 
