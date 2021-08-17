18c98c7f8319e9b61be2ba9d2e96434b5d025370
vacantClaudia <vacantclaudia88@gmail.com>
2021-08-17 18:36:25 +0200

    doubt about the nav

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
-
+// import logo from 'src/assets/pictures/Logo2-monPotager.jpg';
 import './header.scss';
 
-const Header = () => {
-  return (
-    <h1>Ici mon header </h1>
-  );
-};
+const Header = () => (
+  <div className="header">
+    <img
+      // src=
+      alt="logo"
+      className="header-logo"
+    />
+    <div className="nav">
+      <a className="nav_item">Accueil</a>
+      <a className="nav_item">Compte</a>
+      <a className="nav_item">Plantes</a>
+    </div>
+  </div>
+);
 
 export default Header;
