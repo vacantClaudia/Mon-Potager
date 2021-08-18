01c1f6dc2544f96edaf175508fbcb215247e28df
Fouzia33 <fouzia.hollande@gmail.com>
2021-08-18 14:02:33 +0200

    Creation of the static footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 
 import './footer.scss';
 
-const Footer = () => {
-  return (
-    <h1>Ici mon footer</h1>
-  );
-};
+const Footer = () => (
+  <nav className="nav-footer">
+    <ul>
+      <li className="nav-item">
+        <a href="#">Mentions légales</a>
+      </li>
+      <li className="nav-item">
+        <a href="#">CGU</a>
+      </li>
+      <li className="nav-item">
+        <a href="#">Qui sommes-nous?</a>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default Footer;
