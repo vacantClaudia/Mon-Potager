ba298f3a68b58f2af7a22cc1126ce81f979bf7ea
Fouzia33 <fouzia.hollande@gmail.com>
2021-08-26 16:02:44 +0200

    error 404 component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,6 @@
 // == Import npm
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 
 // == Import css
@@ -12,21 +12,36 @@ import Connect from 'src/containers/Connect';
 import VisitorCalendar from 'src/containers/VisitorCalendar';
 import Footer from 'src/components/Footer';
 import PlantsList from 'src/containers/PlantsList';
+import NotFound from 'src/components/NotFound';
 
 // == App component
 const App = () => (
   <div className="app">
+
     <Header />
-    <Route path="/connexion" exact>
-      <Connect />
-    </Route>
-    <Route path="/" exact>
-      <VisitorCalendar />
-    </Route>
-    <Route path="/plantes" exact>
-      <PlantsList />
-    </Route>
+
+    <Switch>
+
+      <Route path="/connexion" exact>
+        <Connect />
+      </Route>
+
+      <Route path="/" exact>
+        <VisitorCalendar />
+      </Route>
+
+      <Route path="/plantes" exact>
+        <PlantsList />
+      </Route>
+
+      <Route>
+        <NotFound />
+      </Route>
+
+    </Switch>
+
     <Footer />
+
   </div>
 );
 
