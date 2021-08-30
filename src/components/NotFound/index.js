3c9be7d68e0ade396f2632ca79ca11f86e126364
Fouzia33 <fouzia.hollande@gmail.com>
2021-08-30 11:47:34 +0200

    update errorPage

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -6,11 +6,46 @@ import './styles.scss';
 
 // == Composant
 const NotFound = () => (
-  <div className="not-found">
-    <img
-      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1RDrvuEqjVEyX0UU2RFwzMYRYJb2A_IvkxA&usqp=CAU"
-      alt="error"
-    />
+  // <div className="not-found">
+  //   <img
+  //     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1RDrvuEqjVEyX0UU2RFwzMYRYJb2A_IvkxA&usqp=CAU"
+  //     alt="error"
+  //   />
+  // </div>
+  <div className="outer-track">
+    <div className="inner-track">
+      <div className="tractor-container">
+        <div className="tractor">
+          <div className="body">
+            <div className="light" />
+            <div className="grill" />
+            <div className="exhaust" />
+            <div className="steering" />
+            <div className="seat" />
+            <div className="wheel-front" />
+            <div className="fender" />
+            <div className="wheel-back" />
+            <div className="hitch" />
+          </div>
+        </div>
+      </div>
+      <div className="rope" />
+      <div className="tractor-container">
+        <div className="tractor">
+          <div className="body">
+            <div className="light" />
+            <div className="grill" />
+            <div className="exhaust" />
+            <div className="steering" />
+            <div className="seat" />
+            <div className="wheel-front" />
+            <div className="fender" />
+            <div className="wheel-back" />
+            <div className="hitch" />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
