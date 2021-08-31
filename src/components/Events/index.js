ad1ede428d29248523fc14f30b8a7a6492ed4a91
vacantClaudia <vacantclaudia88@gmail.com>
2021-08-31 11:59:08 +0200

    filter progress

--- src/components/Events/index.js
+++ src/components/Events/index.js
@@ -7,19 +7,25 @@ const Events = ({ getPlantsList, plantsSchedules }) => {
   useEffect(() => {
     getPlantsList();
   }, []);
+
   // ? les plants par action
   const sowing = plantsSchedules.filter((plant) => plant.period === 'semi');
+  console.log(sowing);
   const planting = plantsSchedules.filter((plant) => plant.period === 'plantation');
   const harvest = plantsSchedules.filter((plant) => plant.period === 'récolte');
 
   // ? les plants par date début
-  const sowingStart = sowing.map((plant) => Date.parse(plant.start));
-  const sowingStartMonth = sowingStart.map((date) => new Date(date));
-  const sowingMonth = sowingStartMonth.map((date) => date.toLocaleString('fr-FR', {
-    timeZone: 'Europe/Paris',
-    month: 'long',
-  }));
+  const sowingMonth = sowing
+    .map((plant) => plant.start)
+    .map((plant) => Date.parse(plant))
+    .map((date) => new Date(date))
+    .map((date) => (date.toLocaleString('fr-FR', {
+      timeZone: 'Europe/Paris',
+      month: 'long',
+    })));
+
   console.log(sowingMonth);
+
   const plantingStart = planting.map((plant) => plant.start);
   const harvestStart = harvest.map((plant) => plant.start);
 
@@ -30,37 +36,76 @@ const Events = ({ getPlantsList, plantsSchedules }) => {
     timeZone: 'Europe/Paris',
     month: 'long',
   });
- console.log('date du jour: ', currentMonth);
+  console.log('date du jour: ', currentMonth);
+
+  const sowingFilterMonth = sowingMonth.filter((plant) => plant === currentMonth);
+console.log(sowingFilterMonth);
   return (
     <div className="events">
       <h2 className="events-title">Les évenements à venir du potager {currentMonth}</h2>
 
       <div className="sowing">
         <h3 className="sowing-title">Les semis à venir </h3>
-        <ul>
-          {sowing.filter((plant) => plant.start === currentMonth + 1)
-            .map((plant) => (
-              <li key={plant.id}>{plant.title}</li>
+        <ul className="plantList">
+
+          {sowing.map((plant) => (
+            <li>{plant.title} {plant.start} </li>
+          ))}
+          {sowing.filter((plant) => plant.start)
+            .map((plant) => Date.parse(plant.start))
+            .map((date) => new Date(date))
+            .map((date) => (
+              <li>
+                {date.toLocaleString('fr-FR', {
+                  timeZone: 'Europe/Paris',
+                  month: 'long',
+                })}
+              </li>
             ))}
         </ul>
       </div>
 
       <div className="planting">
         <h3 className="planting-title">Les plantations à venir</h3>
-        <ul>
+        <ul className="plantList">
+
+
           {planting.map((plant) => (
             <li>{plant.title}</li>
           ))}
+          {planting.filter((plant) => plant.start)
+            .map((plant) => Date.parse(plant.start))
+            .map((date) => new Date(date))
+            .map((date) => (
+              <li>
+                {date.toLocaleString('fr-FR', {
+                  timeZone: 'Europe/Paris',
+                  month: 'long',
+                })}
+              </li>
+            ))}
+
         </ul>
       </div>
 
       <div className="harvest">
         <h3 className="harvest-title">Les récoltes à venir</h3>
         {/* <p>{ plantsSchedules }</p> */}
-        <ul>
+        <ul className="plantList">
           {harvest.map((plant) => (
             <li>{plant.title}</li>
           ))}
+          {harvest.filter((plant) => plant.start)
+            .map((plant) => Date.parse(plant.start))
+            .map((date) => new Date(date))
+            .map((date) => (
+              <li>
+                {date.toLocaleString('fr-FR', {
+                  timeZone: 'Europe/Paris',
+                  month: 'long',
+                })}
+              </li>
+            ))}
         </ul>
       </div>
     </div>
@@ -73,3 +118,18 @@ Events.propTypes = {
 };
 
 export default Events;
+
+/**
+ *   <ul>
+          {sowing.map((plant) => (
+            <li>{plant.title} {plant.start}</li>
+          ))}
+          {sowing.filter((plant) => plant.start)
+            .map((plant) => Date.parse(plant.start))
+            .map((date) => new Date(date))
+            .map((date) => (date.toLocaleString('fr-FR', {
+                  timeZone: 'Europe/Paris',
+                  month: 'long',})
+            ))}
+        </ul>
+ */
