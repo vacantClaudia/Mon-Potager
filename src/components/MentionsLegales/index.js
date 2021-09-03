5474d3ce6c14bbfd3e9d4eb5165c4fb9888e8900
vacantClaudia <vacantclaudia88@gmail.com>
2021-09-03 10:26:46 +0200

    page cgu

--- src/components/MentionsLegales/index.js
+++ src/components/MentionsLegales/index.js
@@ -6,21 +6,17 @@ const MentionsLegales = () => (
     <h2>1 - Édition du site</h2>
     <p>
       En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux utilisateurs du site       internet http://mon-potager.surge.sh/ l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi:
-    </p>
-    <p>
+      <br />
       Propriétaire du site : Claudia, Fouzia, Jonathan, Mathieu et Paul,
-      Contact : claudiavacant@live.fr 0557898372
-      Adresse : chemin du dev
-      33000 BORDEAUX.
-    </p>
-    <p>
-      Identification de l'entreprise : Claudia,Fouzia,Jonathan
-      Mathieu
-      Paul au capital social de € - SIREN : - RCS ou RM : - Adresse postale : chemin du dev
-      33000 BORDEAUX.
-    </p>
-    <p>
-      Hébergeur : Autresurge https://surge.sh/
+      Contact : jardinons@olive.fr 0557898372
+      Adresse : chemin du radis
+      33000 BORD'EAU.
+      <br />
+      Mon Potager
+      Adresse postale : chemin du radis
+      33000 BORD'EAU.
+      <br />
+      Hébergeur : https://surge.sh/
       Autres contributeurs : O'clock
     </p>
     <h2>2 - Propriété intellectuelle et contrefaçons.</h2>
@@ -29,14 +25,12 @@ const MentionsLegales = () => (
       sont les propriétaires des droits de propriété intellectuelle
       et détient les droits d’usage sur tous les éléments accessibles sur le site internet,
       notamment les textes, images, graphismes, logos, vidéos, architecture, icônes et sons.
-    </p>
-    <p>
+      <br />
       Toute reproduction, représentation, modification, publication,
       adaptation de tout ou partie des éléments du site,
       quel que soit le moyen ou le procédé utilisé,est interdite,
       sauf autorisation écrite préalable de Claudia, Fouzia, Jonathan, Mathieu ou Paul.
-    </p>
-    <p>
+      <br />
       Toute exploitation non autorisée du site ou de l’un quelconque
       des éléments qu’il contient sera considérée comme constitutive
       d’une contrefaçon et poursuivie conformément aux dispositions
